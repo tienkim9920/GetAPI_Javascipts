e1aaf04d645f9cbfcd7bccb3fbe56b45c5c2cad3
tienkim9920 <tienkim9920@gmail.com>
2020-12-01 20:25:39 +0700

    API_JavaScipts

--- API_Javascipts/app.js
+++ API_Javascipts/app.js
@@ -1,18 +1,31 @@
 
-var title = document.getElementById('title')
+// var title = document.getElementById('title')
 
-async function postData(url) {
-    const response = await fetch(url, {
-      method: 'GET', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    });
-    return response.json(); 
-  }
+// async function postData(url) {
+//     const response = await fetch(url, {
+//       method: 'GET', // *GET, POST, PUT, DELETE, etc.
+//       mode: 'cors', // no-cors, *cors, same-origin
+
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//     });
+//     return response.json(); 
+//   }
   
-postData('https://jsonplaceholder.typicode.com/posts/2')
-    .then(data => {
-      title.innerHTML = data.body
-});
+// postData('http://localhost:3000/Shopping/products')
+//     .then(data => {
+//       console.log(data)
+// });
+
+fetch('http://localhost:3000/Shopping/products', {
+  //Đây Là POST Data
+  // method: 'POST',
+  // body: JSON.stringify({name: "janith"}),
+
+  //Đây là GET data
+  method: 'GET'
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(err => console.error(err));
